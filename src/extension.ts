9f897d9c0997c2db19b0622c32f0dd163757d161
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-11-05 05:56:16 -0700

    modify panel file

--- src/extension.ts
+++ src/extension.ts
@@ -106,10 +106,21 @@ export function activate(context: vscode.ExtensionContext) {
           )
         ));
 
+        const d3_js = currentPanel.webview.asWebviewUri(vscode.Uri.file(
+          path.join(
+              context.extensionPath,
+              "node_modules",
+              "d3",
+              "dist",
+              "d3.js"
+          )
+        ));
+
         // And set its HTML content
         currentPanel.webview.html = getWebviewContent(
           force_graph_js,
           resize_js,
+          d3_js,
           prev_graph_data
         );
 
@@ -124,129 +135,148 @@ export function activate(context: vscode.ExtensionContext) {
         function getWebviewContent(
           force_graph_js: vscode.Uri,
           resize_js: vscode.Uri,
+          d3_js: vscode.Uri,
           graph_data: Graph_Data
         ) {
+          for (let i = 0; i < graph_data.nodes.length; i++) {
+            graph_data.nodes[i].x = 0;
+            graph_data.nodes[i].y = graph_data.nodes.length - i;
+          }
           return `<head>
-            <style>
-            html, body {
-            height: 100vh;
-            width: 100vw;
-            margin: 0;
-            padding: 0;
-            }
+          <style>
+              html, body {
+              height: 100vh;
+              width: 100vw;
+              margin: 0;
+              padding: 0;
+              }
 
-            * {
-            box-sizing: border-box;
-            }
+              * {
+              box-sizing: border-box;
+              }
 
-            #goalButton {
-            position: absolute; 
-            z-index: 99;
-            left: 0; 
-            right: 0;
-            bottom: 20px;
-            margin-left: auto;
-            margin-right: auto;
-            width: 200px;
-            display: inline-block;
-            outline: 0;
-            border: 1px solid #3d3d3d;
-            cursor: pointer;
-            border-radius: 4px;
-            font-size: 15px;
-            height: 35px;
-            background: #212121;
-            color: white;
-            padding: 0 20px;
-            }
+              #goalButton {
+              position: absolute; 
+              z-index: 99;
+              top: 10px;
+              left: 10px;
+              // left: 0; 
+              // right: 0;
+              // bottom: 20px;
+              // margin-left: auto;
+              // margin-right: auto;
+              width: 120px;
+              display: inline-block;
+              outline: 0;
+              border: 1px solid #3d3d3d;
+              cursor: pointer;
+              border-radius: 0;
+              font-size: 15px;
+              height: 35px;
+              background: #212121;
+              color: white;
+              padding: 0 20px;
+              }
 
-            #goalButton:hover {
-            background-color: #1c1c1c;
-            }
+              #goalButton:hover {
+              background-color: #1c1c1c;
+              }
 
-            
-        </style>
+              
+          </style>
 
-        <script src="${force_graph_js}"></script>
-        <script src="${resize_js}"></script>
+          <script src="${force_graph_js}"></script>
+          <script src="${resize_js}"></script>
+          <script src="${d3_js}"></script>
 
-        </head>
+          </head>
 
-        <body>
-        <button id="goalButton" onClick="toggleGoal()">Show Goal</button>
-        <div id="graph"></div>
+          <body>
+          <button id="goalButton" onClick="toggleGoal()">Show Goal</button>
+          <div id="graph"></div>
 
-        <script>
-            let showGoal = false;
-            const Graph = ForceGraph()
-                (document.getElementById('graph'))
-                .nodeCanvasObject((node, ctx) => nodePaint(node, ['sandybrown', 'lightskyblue', 'hotpink', 'palegreen', 'orchid', 'lightcoral'][node.type], ctx))
-                .nodePointerAreaPaint(nodePaint)
-                .nodeLabel('hover')
-                .backgroundColor('#1a1a1a')
-                .linkDirectionalArrowLength(6)
-                .linkColor(link => 'white')
-                .onNodeRightClick(node => {
-                    navigator.clipboard.writeText(node.rt_clk);
-                })
-                .graphData(${JSON.stringify(graph_data)})
-                            
-            // Handle the message inside the webview
-            window.addEventListener('message', event => {
+          <script>
+              let showGoal = false;
+              let nodeCounter = 0;
+              const Graph = ForceGraph()
+                  (document.getElementById('graph'))
+                  .nodeCanvasObject((node, ctx) => nodePaint(node, ['sandybrown', 'lightskyblue', 'hotpink', 'palegreen', 'orchid', 'lightcoral'][node.type], ctx))
+                  .nodePointerAreaPaint(nodePaint)
+                  .cooldownTicks(300)
+                  .nodeLabel('hover')
+                  .dagMode('radialIn')
+                  .d3Force("link", d3.forceLink().id(function(d) { return d.id; }).distance(20))
+                  .d3Force("charge", d3.forceManyBody().strength(-80))
+                  // .d3Force("x", d3.forceX(.5).strength(.1))
+                  .d3VelocityDecay(.2)
+                  .backgroundColor('#1a1a1a')
+                  .linkDirectionalArrowLength(8)
+                  .linkColor(link => 'white')
+                  .onNodeRightClick(node => {
+                      navigator.clipboard.writeText(node.rt_clk);
+                  })
+                  .graphData(${JSON.stringify(graph_data)})
 
-                let old_graph = Graph.graphData();
-                let new_graph = event.data;
-        
-                for (let i = 0; i < new_graph.nodes.length; ++i) {
-                    for (let j = 0; j < old_graph.nodes.length; ++j) {
-                    if (new_graph.nodes[i].id == old_graph.nodes[j].id) {
-                        new_graph.nodes[i].x = old_graph.nodes[j].x;
-                        new_graph.nodes[i].y = old_graph.nodes[j].y;
-                        new_graph.nodes[i].vx = old_graph.nodes[j].vx;
-                        new_graph.nodes[i].vy = old_graph.nodes[j].vy;                       
-                        break;
-                    }
-                    }
-                }
+              Graph.onEngineStop(() => Graph.zoomToFit(300));
+                              
+              // Handle the message inside the webview
+              window.addEventListener('message', event => {
 
-                Graph.graphData(new_graph);
-                Graph.zoomToFit();
-            });              
+                  let old_graph = Graph.graphData();
+                  let new_graph = event.data;
+          
+                  for (let i = 0; i < new_graph.nodes.length; ++i) {
+                      for (let j = 0; j < old_graph.nodes.length; ++j) {
+                      if (new_graph.nodes[i].id == old_graph.nodes[j].id) {
+                          new_graph.nodes[i].x = old_graph.nodes[j].x;
+                          new_graph.nodes[i].y = old_graph.nodes[j].y;
+                          new_graph.nodes[i].vx = old_graph.nodes[j].vx;
+                          new_graph.nodes[i].vy = old_graph.nodes[j].vy;                       
+                          break;
+                      }
+                      }
+                  }
 
-            function nodePaint({ hover, type, x, y }, color, ctx) {
+                  Graph.graphData(new_graph);
+                  Graph.onEngineStop(() => Graph.zoomToFit(300));
+              });              
 
-                // commit, branch, tag, stash, remote, head
-                let identifier = ['C', 'LB', 'T', 'S', 'RB', 'H'];
-                ctx.fillStyle = color;
+              function nodePaint({ hover, type, x, y }, color, ctx) {
 
-                [
-                () => { ctx.beginPath(); ctx.arc(x, y, 8, 0, 2 * Math.PI, false); ctx.fill(); }, // circle
-                () => { ctx.fillRect(x - 9, y - 9, 18, 18); ctx.fillStyle = 'black'; ctx.font = 'bold 9px Sans-Serif'; ctx.textAlign = 'center'; ctx.textBaseline = 'middle'; ctx.fillText(identifier[type], x, y);}, // text box
-                ][type == 0 ? 0 : 1]();
-            }
-            
-            elementResizeDetectorMaker().listenTo(
-                document.body,
-                (el) => {
-                Graph.width(el.offsetWidth);
-                Graph.height(el.offsetHeight);
-                }
-            );
+                  // commit, branch, tag, stash, remote, head
+                  let identifier = ['C', 'LB', 'T', 'S', 'RB', 'H'];
+                  ctx.fillStyle = color;
 
-            function toggleGoal() {
-                showGoal = !showGoal;
-                const button = document.getElementById("goalButton");
+                  [
+                  () => { ctx.beginPath(); ctx.arc(x, y, 8, 0, 2 * Math.PI, false); ctx.fill(); }, // circle
+                  () => { ctx.fillRect(x - 9, y - 9, 18, 18); ctx.fillStyle = 'black'; ctx.font = 'bold 10px Sans-Serif'; ctx.textAlign = 'center'; ctx.textBaseline = 'middle'; ctx.fillText(identifier[type], x, y);}, // text box
+                  ][type == 0 ? 0 : 1]();
+              }
+              
+              elementResizeDetectorMaker().listenTo(
+                  document.body,
+                  (el) => {
+                      Graph.width(el.offsetWidth);
+                      Graph.height(el.offsetHeight);
+                      Graph.d3ReheatSimulation();
+                      Graph.onEngineStop(() => Graph.zoomToFit(300));
+                  }
+              );
 
-                if (showGoal) {
-                button.innerHTML = 'Hide Goal';
-                } else {
-                    button.innerHTML = 'Show Goal';
-                }
-            }
-        </script>
-        </body>`;
+              function toggleGoal() {
+                  showGoal = !showGoal;
+                  const button = document.getElementById("goalButton");
+
+                  if (showGoal) {
+                  button.innerHTML = 'Hide Goal';
+                  } else {
+                      button.innerHTML = 'Show Goal';
+                  }
+              }
+          </script>
+          </body>`;
+          }
         }
-      }
      }
   );
 
