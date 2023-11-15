5ebb1dd04f713a99303a205ae20c1c5f79bac4ab
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-11-14 19:02:26 -0700

    remove autograder functionality

--- src/provider.ts
+++ src/provider.ts
@@ -9,8 +9,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
 	private _view?: vscode.WebviewView;
     private prevCommits?: LogResult<DefaultLogFields>;
     private prev_graph_data: Graph_Data | undefined;
-    private sol_graph_data: Graph_Data | undefined;
-    private completed: boolean;
 
 
 	constructor(
@@ -18,30 +16,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
 	) {
         this.prevCommits = undefined;
         this.prev_graph_data = undefined;
-        this.sol_graph_data = undefined;
-        this.completed = false;
-     }
-
-    private async renameFolder(path: string, oldFolder: string, newFolder: string) {
-        const oldFolderPath = vscode.Uri.file(path + "/" + oldFolder); // Replace with the old folder path
-        const newFolderPath = vscode.Uri.file(path + "/" + newFolder); // Replace with the new folder path
-
-        try {
-            const parentDirectoryContents = await vscode.workspace.fs.readDirectory(vscode.Uri.file(path));
-            const oldFolderExists = parentDirectoryContents.some(([name, type]) => name === oldFolder && type === vscode.FileType.Directory);
-    
-            if (!oldFolderExists) {
-                // vscode.window.showErrorMessage('Error renaming folder: The old folder does not exist.');
-                return;
-            }
-    
-            // Rename the folder if it exists
-            await vscode.workspace.fs.rename(oldFolderPath, newFolderPath);
-            // vscode.window.showInformationMessage('Folder renamed successfully.');
-        } catch (error: any) {
-            // vscode.window.showErrorMessage('Error renaming folder: ' + error.message);
-            return;
-        }
     }
 
 	public async resolveWebviewView(
@@ -91,6 +65,9 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
         // get path of workspace (first workspace)
         let ws_path = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
+        // get git graph data
+        this.prev_graph_data = await get_git_graph(ws_path);
+
         // // check if commits have changed
 
         // let currCommits: LogResult<DefaultLogFields> = await get_commits(ws_path);
@@ -100,13 +77,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
         // }
 
         // this.prevCommits = currCommits;
-
-
-        // get git graph data
-        this.prev_graph_data = await get_git_graph(ws_path);
-        await this.renameFolder(ws_path + "/.Git-Gud", ".git-gud", ".git");
-        this.sol_graph_data = await get_git_graph(ws_path + "/.Git-Gud")
-        await this.renameFolder(ws_path + "/.Git-Gud", ".git", ".git-gud");
 
         // Get path to resource on disk
         // And get the special URI to use with the webview
@@ -146,8 +116,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
             resize_js,
             d3_js,
             this.prev_graph_data,
-            this.sol_graph_data,
-            this.completed,
         );
 
         function getWebviewContent(
@@ -155,16 +123,10 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
             resize_js: vscode.Uri,
             d3_js: vscode.Uri,
             graph_data: Graph_Data,
-            sol_graph_data: Graph_Data,
-            completed: boolean,
         ) {
         for (let i = 0; i < graph_data.nodes.length; i++) {
             graph_data.nodes[i].x = 0;
             graph_data.nodes[i].y = (graph_data.nodes.length - i);
-        }
-        for (let i = 0; i < sol_graph_data.nodes.length; i++) {
-            sol_graph_data.nodes[i].x = 0;
-            sol_graph_data.nodes[i].y = (sol_graph_data.nodes.length - i);
         }
         return `<head>
         <style>
@@ -178,49 +140,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
             * {
             box-sizing: border-box;
             }
-
-            #goalButton {
-            position: absolute; 
-            z-index: 99;
-            top: 10px;
-            left: 10px;
-            // left: 0; 
-            // right: 0;
-            // bottom: 20px;
-            // margin-left: auto;
-            // margin-right: auto;
-            width: 120px;
-            display: inline-block;
-            outline: 0;
-            border: 1px solid #3d3d3d;
-            cursor: pointer;
-            border-radius: 0;
-            font-size: 15px;
-            height: 35px;
-            background: #212121;
-            color: white;
-            padding: 0 20px;
-            }
-
-            #goalButton:hover {
-            background-color: #1c1c1c;
-            }
-
-            #completedText {
-                position: absolute;
-                text-align: center;
-                z-index: 99;
-                top: 35px;
-                left: 0;
-                right: 0;
-                margin-left: auto;
-                margin-right: auto;
-                // padding-top: 10px;
-                // padding-bottom: 10px;
-                font-size: 30px;
-                font-weight: bold;
-                color: lightgreen;
-            }
         </style>
 
         <script src="${force_graph_js}"></script>
@@ -230,16 +149,10 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
         </head>
 
         <body>
-        <button id="goalButton" onClick="toggleGoal()">Show Goal</button>
-        <p id="completedText">Exercise Completed!</p>
         <div id="graph"></div>
 
         <script>
-            let showGoal = false;
-            let nodeCounter = 0;
             let graphData = ${JSON.stringify(graph_data)};
-            let solGraphData = ${JSON.stringify(sol_graph_data)};
-            let completed = ${JSON.stringify(completed)};
             const Graph = ForceGraph()
                 (document.getElementById('graph'))
                 .nodeCanvasObject((node, ctx) => nodePaint(node, ['sandybrown', 'lightskyblue', 'hotpink', 'palegreen', 'orchid', 'lightcoral'][node.type], ctx))
@@ -257,38 +170,12 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
                     navigator.clipboard.writeText(node.rt_clk);
                 })
 
-                // .d3VelocityDecay(.2)
-
-            if (showGoal) {
-                Graph.graphData(solGraphData);
-            } else {
-                Graph.graphData(graphData);
-            }
-
-            let completedText = document.getElementById("completedText");
-            if (completed) {
-                completedText.style.display = "block";
-              booleanValue = false;
-            } else {
-                completedText.style.display = "none";
-            }
-
+            Graph.graphData(graphData);
             Graph.onEngineStop(() => Graph.zoomToFit(200, 50));
+            // setTimeout(Graph.zoomToFit(200, 50), 500);
                             
             // Handle the message inside the webview
             window.addEventListener('message', event => {
-                if (event.data === false || event.data === true) {
-                    completed = event.data;
-                    let completedText = document.getElementById("completedText");
-                    if (completed) {
-                        completedText.style.display = "block";
-                      booleanValue = false;
-                    } else {
-                        completedText.style.display = "none";
-                    }
-                    return;
-                }
-
                 let old_graph = Graph.graphData();
                 graphData = event.data;
         
@@ -304,12 +191,8 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
                     }
                 }
 
-                if (showGoal) {
-                    Graph.graphData(solGraphData);
-                } else {
-                    Graph.graphData(graphData);
-                }
-                Graph.onEngineStop(() => Graph.zoomToFit(200, 50));
+                Graph.graphData(graphData);
+                // Graph.zoomToFit(200, 50);
             });              
 
             function nodePaint({ hover, type, x, y }, color, ctx) {
@@ -330,25 +213,9 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
                     Graph.width(el.offsetWidth);
                     Graph.height(el.offsetHeight);
                     Graph.d3ReheatSimulation();
-                    Graph.onEngineStop(() => Graph.zoomToFit(200, 50));
+                    // Graph.zoomToFit(200, 50);
                 }
             );
-
-            function toggleGoal() {
-                showGoal = !showGoal;
-                const button = document.getElementById("goalButton");
-
-                if (showGoal) {
-                button.innerHTML = 'Hide Goal';
-                } else {
-                    button.innerHTML = 'Show Goal';
-                }
-                if (showGoal) {
-                    Graph.graphData(solGraphData);
-                } else {
-                    Graph.graphData(graphData);
-                }
-            }
         </script>
         </body>`;
         }
@@ -356,7 +223,7 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
     
     public async updateGraphData(context: vscode.ExtensionContext) {
         // makes sure that only 1 workspace is open
-        if (vscode.workspace.workspaceFolders == undefined) {
+        if (vscode.workspace.workspaceFolders === undefined) {
             vscode.window.showInformationMessage(
               "No workspace opened! Please open only 1 workspace."
             );
@@ -388,19 +255,6 @@ export class GitVisualizerProvider implements vscode.WebviewViewProvider {
             curr_graph_data.nodes[i].x = 0;
             curr_graph_data.nodes[i].y = (curr_graph_data.nodes.length - i);
         }
-
-        this.completed = false;
-        if (this.sol_graph_data && curr_graph_data.nodes.length === this.sol_graph_data.nodes.length) {
-            this.completed = true;
-            for (let i = 0; i < curr_graph_data.nodes.length; i++) {
-                if (curr_graph_data.nodes[i].hover !== this.sol_graph_data.nodes[i].hover) {
-                    this.completed = false;
-                    break;
-                }
-            }
-        }
-
-        this._view!.webview.postMessage(this.completed);
     
         if (this.prev_graph_data && JSON.stringify(curr_graph_data.nodes) === JSON.stringify(this.prev_graph_data.nodes)) {
             return;
